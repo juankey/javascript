8d520c3c7f564a6496d266f661a729cc6211d0e2
juankey <solojcarlos@hotmail.com>
2021-11-14 19:40:05 -0500

    hola

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-let uno="hola mundo";
-prompt(uno);
+let hola = "hola mundo"
+console.log(hola);
+const hola2 = 1;
+console.log(hola + " " +hola2);
+
+function holaMundo2(){
+    console.log("peru");
+}
+holaMundo2();
+
+let cadena = [true,2,3,"hola"];
+
+console.log(cadena[4]);
